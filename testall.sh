97d02a002e40b5c1f6a9f1db35c0dfebc6422ea1
njtran <nichotr@amazon.com>
2023-08-11 19:35:13 -0700

    test script

---
+++ testall.sh
@@ -0,0 +1,14 @@
+echo "starting small"
+../ccmon ../bin/ccmon/small.yaml
+
+echo "starting medium"
+../ccmon ../bin/ccmon/medium.yaml
+
+echo "starting large"
+../ccmon ../bin/ccmon/large.yaml
+
+echo "starting xlarge"
+../ccmon ../bin/ccmon/xlarge.yaml
+
+echo "starting 2xlarge"
+../ccmon ../bin/ccmon/2xlarge.yaml
